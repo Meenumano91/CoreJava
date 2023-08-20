342e39318ae031039d67c556a2711817891d3206
meenu <drmvsm1991@gmail.com>
2023-08-20 15:36:46 +0530

    cleared the main class

--- src/Test.java
+++ src/Test.java
@@ -4,30 +4,39 @@ import Inheritance.Dog;
 
 public class Test {
     public static void main(String[] args) {
-        Animal animal=new Animal();
-        Animal cat=new Cat();
-        Animal dog=new Dog();
 
-        Dog dog1=new Dog();
-        Cat cat1=new Cat();
 
-        animal.makeSound();
-        animal.eat();
 
-        cat.makeSound();
-        cat.eat();
+//        testInheritance();
+    }
 
-        dog.makeSound();
+    private static void testInheritance() {
 
-       dog.eat();
-
-        cat1.makeSound();
-        cat1.eat();
-
-        dog1.makeSound();
-        dog1.eat();
-
-        dog.run();
+        // Inheritance
+//        Animal animal=new Animal();
+//        Animal cat=new Cat();
+//        Animal dog=new Dog();
+//
+//        Dog dog1=new Dog();
+//        Cat cat1=new Cat();
+//
+//        animal.makeSound();
+//        animal.eat();
+//
+//        cat.makeSound();
+//        cat.eat();
+//
+//        dog.makeSound();
+//
+//       dog.eat();
+//
+//        cat1.makeSound();
+//        cat1.eat();
+//
+//        dog1.makeSound();
+//        dog1.eat();
+//
+//        dog.run();
     }
 
 }
