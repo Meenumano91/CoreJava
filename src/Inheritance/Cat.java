d960b5a64b7983b9d42638d51450df887860460c
meenu <drmvsm1991@gmail.com>
2023-08-20 15:01:51 +0530

    Implemented cat class

--- src/Inheritance/Cat.java
+++ src/Inheritance/Cat.java
@@ -1,4 +1,19 @@
 package Inheritance;
 
 public class Cat extends Animal{
+
+    public void makeSound()
+    {
+        System.out.println("Cat meow");
+    }
+
+    public void Home()
+    {
+        System.out.println("Cat lives in a street");
+    }
+
+    public void eat()
+    {
+        System.out.println("Cat eat fish");
+    }
 }
