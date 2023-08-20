4f9f86f41761c79edb96d084a293e883bb1cf8d7
meenu <drmvsm1991@gmail.com>
2023-08-20 14:58:48 +0530

    Implemented animal class

--- src/Inheritance/Animal.java
+++ src/Inheritance/Animal.java
@@ -1,5 +1,13 @@
 package Inheritance;
 
 public class Animal {
+    String species;
 
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+
+    public void eat() {
+        System.out.println("Animal eats meat");
+    }
 }
